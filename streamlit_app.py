0f1664686176ee62abbf938d429836b99111eff0
PreethiKolipaka2000 <86162886+PreethiKolipaka2000@users.noreply.github.com>
2023-12-30 19:41:48 +0530

    Update streamlit_app.py
    import another package

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit 
 import pandas
+import requests
 
 streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omego 3 and Blueberry Oatmeal") 
@@ -12,7 +13,9 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index("Fruit")
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.dataframe(fruits_to_show)
+streamlist.text(fruityvice_response)
 
 
